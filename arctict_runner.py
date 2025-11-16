025ea8abff8d2f4b77e6cf0f801dff8bde56deef
sina <sina.behnam.ac@gmail.com>
2025-11-16 17:36:32 +0100

    add arctic runner

--- arctict_runner.py
+++ arctict_runner.py
@@ -1,64 +1,12 @@
 import os
 import sys
-from src.utils.templates.base import BasePromptTemplate
+from src.utils.templates.arctic import ArcticText2SQLTemplate
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
 from src.dataloader import DataLoader, Text2SQLDataset
 import argparse
 from tqdm import tqdm
 
-input_prompt_template = '''Task Overview:
-You are a data science expert. Below, you are provided with a database schema and a natural language question. Your task is to understand the schema and generate a valid SQL query to answer the question.
-
-Database Engine:
-SQLite
-
-Database Schema:
-{db_details}
-This schema describes the database's structure, including tables, columns, primary keys, foreign keys, and any relevant relationships or constraints.
-
-Question:
-{question}
-{evidence}
-
-Instructions:
-- Make sure you only output the information that is asked in the question. If the question asks for a specific column, make sure to only include that column in the SELECT clause, nothing more.
-- The generated query should return all of the information asked in the question without any missing or extra information.
-- Before generating the final SQL query, please think through the steps of how to write the query.
-
-Output Format:
-In your answer, please enclose the generated SQL query in a code block:
-```
--- Your SQL query
-```
-
-Take a deep breath and think step by step to find the correct SQL query.'''
-
-
-class OmniSQLPromptTemplate(BasePromptTemplate):
-
-    def create_prompt(self, question, schema, dialect=None, evidence = None):
-        
-        usr_msg = input_prompt_template.format(
-            db_details = schema,
-            question = question,
-            evidence = '' if evidence is None else f'(PS :\n{evidence})'
-        )
-
-        return '', usr_msg, ''
-    
-    def extract_sql(self, response_text, clean = True):
-        
-        extractor = self.SQLExtractorHelper()
-
-        sql = extractor._try_extraction_methods([
-            extractor._try_code_block_extraction,
-        ],response_text)
-        
-        if self._is_valid_sql(sql):
-            return self._clean_sql(sql)
-        
-        return None
     
 def process_logprobs(logprobs, num_logprobs):
 
@@ -78,7 +26,6 @@ def process_logprobs(logprobs, num_logprobs):
     return _logprobs
         
             
-
 def generate_batch(batched_instances, llm ,tokenizer, sampling_params):
     # Extract instance IDs and user messages
     instances_ids = batched_instances['instance_id'].detach().cpu().tolist()
@@ -110,10 +57,10 @@ def generate_batch(batched_instances, llm ,tokenizer, sampling_params):
 
     return batch_responses
 
-def downaload_model(local_dir="./local_models/OmniSQL-7B"):
+def downaload_model(local_dir=""):
     from huggingface_hub import snapshot_download
 
-    model_name = "seeklhy/OmniSQL-7B"
+    model_name = "Snowflake/Arctic-Text2SQL-R1-7B"
 
     print("Downloading model...")
     snapshot_download(
@@ -187,7 +134,7 @@ def main():
     presence_penalty = args.pp
     logprobs = args.logprobs
 
-    prompt_template = OmniSQLPromptTemplate()
+    prompt_template = ArcticText2SQLTemplate()
 
     dataset = Text2SQLDataset(data_path, template=prompt_template, dialect=dialect)
 
