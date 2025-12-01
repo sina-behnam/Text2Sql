caa32946afa121b31651639072fcef5c094c0e49
sina <sina.behnam.ac@gmail.com>
2025-12-01 10:45:56 +0100

    adding find_by_id and compute many as abstract method

--- src/evaluation/metrics/metric.py
+++ src/evaluation/metrics/metric.py
@@ -11,6 +11,8 @@ from typing import Any, Dict
 from abc import ABC, abstractmethod
 from enum import Enum
 from src.typing.metrics import MetricType
+from src.typing.query import DBQuery
+from typing import List
     
 class Metric(ABC):
     name: MetricType = None
@@ -19,9 +21,20 @@ class Metric(ABC):
     @classmethod
     def get_name(cls) -> MetricType:
         return cls.name
+    
+    @abstractmethod
+    def find_by_id(self, target: List[DBQuery], id: str) -> Any:
+        """Find an instance by its ID.
+
+        Args:
+            id (str): The ID of the instance to find.
+        Returns:
+            Any: The found instance or None if not found.
+        """
+        pass
 
     @abstractmethod
-    def compute(self, target, prediction) -> Dict[str, float | int]:
+    def compute(self, target : DBQuery, prediction: DBQuery) -> float | int:
         """Compute the metric value given target and prediction.
 
         Args:
@@ -29,6 +42,18 @@ class Metric(ABC):
             prediction (Any): The predicted data.
         Returns:
             Dict[str, float | int]: The computed metric values.
+        """
+        pass
+
+    @abstractmethod
+    def compute_many(self, target: list[DBQuery], prediction: list[DBQuery]) -> list[float | int]:
+        """Compute the metric values for multiple target-prediction pairs.
+
+        Args:
+            target (List[Any]): List of ground truth data.
+            prediction (List[Any]): List of predicted data.
+        Returns:
+            List[Dict[str, float | int]]: List of computed metric values.
         """
         pass
 
