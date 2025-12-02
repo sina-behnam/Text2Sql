5f94eddecf98ac8703121b13b9639abfa8f18c75
sina <sina.behnam.ac@gmail.com>
2025-12-02 21:29:48 +0100

    init main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from typing import Optional
 PROJECT_ROOT = Path(__file__).parent
 sys.path.insert(0, str(PROJECT_ROOT))
 
-from src.dataloader import DatasetLoader
+from loaders.dataloader import DatasetLoader
 from pipeline.modular_pipeline import Text2SQLPipeline
 from pipeline.steps import (
     GenerateStep, ExtractStep, EvaluateStep, MetricsStep, SaveStep, LoadStep
