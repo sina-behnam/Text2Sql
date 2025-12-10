2f51769547c5573462e56860c0abec3431834869
sina <sina.behnam.ac@gmail.com>
2025-12-10 14:04:19 +0100

    adding metrics types

--- src/typing/metrics.py
+++ src/typing/metrics.py
@@ -9,13 +9,24 @@ class ExecutionLevelMetricType(str, Enum):
     EXECUTION_ACCURACY = "execution_accuracy"
     EXECUTION_TIME = "execution_time"
     VALID_EFFICIENCY_SCORE = "valid_efficiency_score"
+    EXECUTION_LATENCY = "execution_latency"
+
+
+class EfficiencyMetricType(str, Enum):
+    """Metrics for evaluating query efficiency beyond simple latency."""
+    QUERY_PLAN_SCORE = "query_plan_score"
+    QUERY_COMPLEXITY = "query_complexity"
+    RESULT_SET_EFFICIENCY = "result_set_efficiency"
+    MEMORY_EFFICIENCY = "memory_efficiency"
 
 class CellLevelMetricType(str, Enum):
     CELL_PRECISION = "cell_precision"
     CELL_RECALL = "cell_recall"
 
-class ExactMatchMetricType(str, Enum):
+class MatchingMetricType(str, Enum):
     EXACT_MATCH = "exact_match"
+    PROPORTIONAL_EXACT_MATCH = "proportional_exact_match"
+    COMPONENT_MATCHING = "component_matching"
 
 class MetricType(str, Enum):
     # Execution Level Metrics
@@ -33,4 +44,12 @@ class MetricType(str, Enum):
     TUPLE_CONSTRAINTS = TupleLevelMetricType.TUPLE_CONSTRAINTS
 
     # Exact Match Metric
-    EXACT_MATCH = ExactMatchMetricType.EXACT_MATCH
+    EXACT_MATCH = MatchingMetricType.EXACT_MATCH
+    PROPORTIONAL_EXACT_MATCH = MatchingMetricType.PROPORTIONAL_EXACT_MATCH
+    COMPONENT_MATCHING = MatchingMetricType.COMPONENT_MATCHING
+
+    # Efficiency Metrics
+    QUERY_PLAN_SCORE = EfficiencyMetricType.QUERY_PLAN_SCORE
+    QUERY_COMPLEXITY = EfficiencyMetricType.QUERY_COMPLEXITY
+    RESULT_SET_EFFICIENCY = EfficiencyMetricType.RESULT_SET_EFFICIENCY
+    MEMORY_EFFICIENCY = EfficiencyMetricType.MEMORY_EFFICIENCY
